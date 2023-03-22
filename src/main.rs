8858291f7e14a6d366b3e3e62437b13037bc0c72
nvarner <nathanmvarner@protonmail.com>
2023-03-22 04:04:34 -0400

    preview one file as a PDF

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
+use std::fs;
+use std::sync::Arc;
+
+use system_world::SystemWorld;
+use tokio::sync::RwLock;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-#[derive(Debug)]
+mod system_world;
+
 struct Backend {
     client: Client,
+    world: Arc<RwLock<Option<SystemWorld>>>,
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+    async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
+        let mut world = self.world.write().await;
+        *world = Some(SystemWorld::new(
+            params.root_uri.unwrap().to_file_path().unwrap(),
+            String::new(),
+        ));
+
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
                 completion_provider: Some(CompletionOptions::default()),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
                 ..Default::default()
             },
             ..Default::default()
@@ -28,6 +44,35 @@ impl LanguageServer for Backend {
 
     async fn shutdown(&self) -> Result<()> {
         Ok(())
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        let mut world = self.world.write().await;
+        let world = world.as_mut().unwrap();
+        let text = &params.content_changes[0].text;
+        world.update_main_source(text.clone());
+
+        let output_path = params
+            .text_document
+            .uri
+            .to_file_path()
+            .unwrap()
+            .with_extension("pdf");
+
+        let output = match typst::compile(world) {
+            Ok(document) => {
+                let buffer = typst::export::pdf(&document);
+                fs::write(output_path, buffer).map_err(|_| "failed to write PDF file".to_string())
+            }
+            Err(errors) => {
+                let messages: Vec<_> = errors.iter().map(|error| error.message.as_str()).collect();
+                Err(messages.join("\n"))
+            }
+        };
+
+        self.client
+            .log_message(MessageType::INFO, format!("{:?}", output))
+            .await;
     }
 
     async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
@@ -50,6 +95,9 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        world: Arc::new(RwLock::new(None)),
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
---
+++ src/system_world.rs
@@ -0,0 +1,352 @@
+//! Copied from the Typst CLI because it is not exported.
+//!
+//! Modified for `Send`/`Sync` support.
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::hash::Hash;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
+use append_only_vec::AppendOnlyVec;
+use comemo::Prehashed;
+use memmap2::Mmap;
+use once_cell::sync::OnceCell;
+use parking_lot::{MappedRwLockWriteGuard, RwLock, RwLockWriteGuard};
+use same_file::Handle;
+use siphasher::sip128::{Hasher128, SipHasher};
+use typst::diag::{FileError, FileResult};
+use typst::eval::Library;
+use typst::font::{Font, FontBook, FontInfo};
+use typst::syntax::{Source, SourceId};
+use typst::util::{Buffer, PathExt};
+use typst::World;
+use walkdir::WalkDir;
+
+type CodespanResult<T> = Result<T, CodespanError>;
+type CodespanError = codespan_reporting::files::Error;
+
+/// A world that provides access to the operating system.
+pub struct SystemWorld {
+    root: PathBuf,
+    library: Prehashed<Library>,
+    book: Prehashed<FontBook>,
+    fonts: Vec<FontSlot>,
+    hashes: RwLock<HashMap<PathBuf, FileResult<PathHash>>>,
+    paths: RwLock<HashMap<PathHash, PathSlot>>,
+    sources: AppendOnlyVec<Source>,
+    main: SourceId,
+    main_source: Source,
+}
+
+/// Holds details about the location of a font and lazily the font itself.
+struct FontSlot {
+    path: PathBuf,
+    index: u32,
+    font: OnceCell<Option<Font>>,
+}
+
+/// Holds canonical data for all paths pointing to the same entity.
+#[derive(Default)]
+struct PathSlot {
+    source: OnceCell<FileResult<SourceId>>,
+    buffer: OnceCell<FileResult<Buffer>>,
+}
+
+impl SystemWorld {
+    pub fn new(root: PathBuf, main_source: String) -> Self {
+        let mut searcher = FontSearcher::new();
+        searcher.search_system();
+
+        Self {
+            root,
+            library: Prehashed::new(typst_library::build()),
+            book: Prehashed::new(searcher.book),
+            fonts: searcher.fonts,
+            hashes: RwLock::default(),
+            paths: RwLock::default(),
+            sources: AppendOnlyVec::new(),
+            main: SourceId::detached(),
+            main_source: Source::detached(main_source),
+        }
+    }
+}
+
+impl World for SystemWorld {
+    fn root(&self) -> &Path {
+        &self.root
+    }
+
+    fn library(&self) -> &Prehashed<Library> {
+        &self.library
+    }
+
+    fn main(&self) -> &Source {
+        self.source(self.main)
+    }
+
+    fn resolve(&self, path: &Path) -> FileResult<SourceId> {
+        self.slot(path)?
+            .source
+            .get_or_init(|| {
+                let buf = read(path)?;
+                let text = String::from_utf8(buf)?;
+                Ok(self.insert(path, text))
+            })
+            .clone()
+    }
+
+    fn source(&self, id: SourceId) -> &Source {
+        if id != self.main {
+            &self.sources[id.into_u16() as usize]
+        } else {
+            &self.main_source
+        }
+    }
+
+    fn book(&self) -> &Prehashed<FontBook> {
+        &self.book
+    }
+
+    fn font(&self, id: usize) -> Option<Font> {
+        let slot = &self.fonts[id];
+        slot.font
+            .get_or_init(|| {
+                let data = self.file(&slot.path).ok()?;
+                Font::new(data, slot.index)
+            })
+            .clone()
+    }
+
+    fn file(&self, path: &Path) -> FileResult<Buffer> {
+        self.slot(path)?
+            .buffer
+            .get_or_init(|| read(path).map(Buffer::from))
+            .clone()
+    }
+}
+
+impl SystemWorld {
+    pub fn update_main_source(&mut self, main_source: String) {
+        self.main_source.replace(main_source);
+    }
+
+    fn slot(&self, path: &Path) -> FileResult<MappedRwLockWriteGuard<PathSlot>> {
+        let mut hashes = self.hashes.write();
+        let hash = match hashes.get(path).cloned() {
+            Some(hash) => hash,
+            None => {
+                let hash = PathHash::new(path);
+                if let Ok(canon) = path.canonicalize() {
+                    hashes.insert(canon.normalize(), hash.clone());
+                }
+                hashes.insert(path.into(), hash.clone());
+                hash
+            }
+        }?;
+
+        Ok(RwLockWriteGuard::map(self.paths.write(), |path| {
+            path.entry(hash).or_default()
+        }))
+    }
+
+    fn insert(&self, path: &Path, text: String) -> SourceId {
+        let id = SourceId::from_u16(self.sources.len() as u16);
+        let source = Source::new(id, path, text);
+        self.sources.push(source);
+        id
+    }
+
+    fn relevant(&mut self, event: &notify::Event) -> bool {
+        match &event.kind {
+            notify::EventKind::Any => {}
+            notify::EventKind::Access(_) => return false,
+            notify::EventKind::Create(_) => return true,
+            notify::EventKind::Modify(kind) => match kind {
+                notify::event::ModifyKind::Any => {}
+                notify::event::ModifyKind::Data(_) => {}
+                notify::event::ModifyKind::Metadata(_) => return false,
+                notify::event::ModifyKind::Name(_) => return true,
+                notify::event::ModifyKind::Other => return false,
+            },
+            notify::EventKind::Remove(_) => {}
+            notify::EventKind::Other => return false,
+        }
+
+        event.paths.iter().any(|path| self.dependant(path))
+    }
+
+    fn dependant(&self, path: &Path) -> bool {
+        self.hashes.read().contains_key(&path.normalize())
+            || PathHash::new(path).map_or(false, |hash| self.paths.read().contains_key(&hash))
+    }
+
+    fn reset(&mut self) {
+        self.sources = AppendOnlyVec::new();
+        self.hashes.get_mut().clear();
+        self.paths.get_mut().clear();
+    }
+}
+
+/// Read a file.
+fn read(path: &Path) -> FileResult<Vec<u8>> {
+    let f = |e| FileError::from_io(e, path);
+    let mut file = File::open(path).map_err(f)?;
+    if file.metadata().map_err(f)?.is_file() {
+        let mut data = vec![];
+        file.read_to_end(&mut data).map_err(f)?;
+        Ok(data)
+    } else {
+        Err(FileError::IsDirectory)
+    }
+}
+
+impl<'a> codespan_reporting::files::Files<'a> for SystemWorld {
+    type FileId = SourceId;
+    type Name = std::path::Display<'a>;
+    type Source = &'a str;
+
+    fn name(&'a self, id: SourceId) -> CodespanResult<Self::Name> {
+        Ok(World::source(self, id).path().display())
+    }
+
+    fn source(&'a self, id: SourceId) -> CodespanResult<Self::Source> {
+        Ok(World::source(self, id).text())
+    }
+
+    fn line_index(&'a self, id: SourceId, given: usize) -> CodespanResult<usize> {
+        let source = World::source(self, id);
+        source
+            .byte_to_line(given)
+            .ok_or_else(|| CodespanError::IndexTooLarge {
+                given,
+                max: source.len_bytes(),
+            })
+    }
+
+    fn line_range(&'a self, id: SourceId, given: usize) -> CodespanResult<std::ops::Range<usize>> {
+        let source = World::source(self, id);
+        source
+            .line_to_range(given)
+            .ok_or_else(|| CodespanError::LineTooLarge {
+                given,
+                max: source.len_lines(),
+            })
+    }
+
+    fn column_number(&'a self, id: SourceId, _: usize, given: usize) -> CodespanResult<usize> {
+        let source = World::source(self, id);
+        source.byte_to_column(given).ok_or_else(|| {
+            let max = source.len_bytes();
+            if given <= max {
+                CodespanError::InvalidCharBoundary { given }
+            } else {
+                CodespanError::IndexTooLarge { given, max }
+            }
+        })
+    }
+}
+
+/// Searches for fonts.
+struct FontSearcher {
+    book: FontBook,
+    fonts: Vec<FontSlot>,
+}
+
+impl FontSearcher {
+    /// Create a new, empty system searcher.
+    fn new() -> Self {
+        Self {
+            book: FontBook::new(),
+            fonts: vec![],
+        }
+    }
+
+    /// Search for fonts in the linux system font directories.
+    #[cfg(all(unix, not(target_os = "macos")))]
+    fn search_system(&mut self) {
+        self.search_dir("/usr/share/fonts");
+        self.search_dir("/usr/local/share/fonts");
+
+        if let Some(dir) = dirs::font_dir() {
+            self.search_dir(dir);
+        }
+    }
+
+    /// Search for fonts in the macOS system font directories.
+    #[cfg(target_os = "macos")]
+    fn search_system(&mut self) {
+        self.search_dir("/Library/Fonts");
+        self.search_dir("/Network/Library/Fonts");
+        self.search_dir("/System/Library/Fonts");
+
+        if let Some(dir) = dirs::font_dir() {
+            self.search_dir(dir);
+        }
+    }
+
+    /// Search for fonts in the Windows system font directories.
+    #[cfg(windows)]
+    fn search_system(&mut self) {
+        let windir = std::env::var("WINDIR").unwrap_or_else(|_| "C:\\Windows".to_string());
+
+        self.search_dir(Path::new(&windir).join("Fonts"));
+
+        if let Some(roaming) = dirs::config_dir() {
+            self.search_dir(roaming.join("Microsoft\\Windows\\Fonts"));
+        }
+
+        if let Some(local) = dirs::cache_dir() {
+            self.search_dir(local.join("Microsoft\\Windows\\Fonts"));
+        }
+    }
+
+    /// Search for all fonts in a directory recursively.
+    fn search_dir(&mut self, path: impl AsRef<Path>) {
+        for entry in WalkDir::new(path)
+            .follow_links(true)
+            .sort_by(|a, b| a.file_name().cmp(b.file_name()))
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
+            let path = entry.path();
+            if matches!(
+                path.extension().and_then(|s| s.to_str()),
+                Some("ttf" | "otf" | "TTF" | "OTF" | "ttc" | "otc" | "TTC" | "OTC"),
+            ) {
+                self.search_file(path);
+            }
+        }
+    }
+
+    /// Index the fonts in the file at the given path.
+    fn search_file(&mut self, path: impl AsRef<Path>) {
+        let path = path.as_ref();
+        if let Ok(file) = File::open(path) {
+            if let Ok(mmap) = unsafe { Mmap::map(&file) } {
+                for (i, info) in FontInfo::iter(&mmap).enumerate() {
+                    self.book.push(info);
+                    self.fonts.push(FontSlot {
+                        path: path.into(),
+                        index: i as u32,
+                        font: OnceCell::new(),
+                    });
+                }
+            }
+        }
+    }
+}
+
+/// A hash that is the same for all paths pointing to the same entity.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+struct PathHash(u128);
+
+impl PathHash {
+    fn new(path: &Path) -> FileResult<Self> {
+        let f = |e| FileError::from_io(e, path);
+        let handle = Handle::from_path(path).map_err(f)?;
+        let mut state = SipHasher::new();
+        handle.hash(&mut state);
+        Ok(Self(state.finish128().as_u128()))
+    }
+}
