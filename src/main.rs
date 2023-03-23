c9a922feca429694b8138a44b0661b973d37678c
Astrale_works <ash4567@outlook.fr>
2023-03-23 10:50:57 +0100

    adds basic error diagnostics

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use tokio::sync::RwLock;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+use typst::diag::SourceError;
+use typst::World;
+use typst::syntax::Source;
 
 mod system_world;
 
@@ -58,20 +61,32 @@ impl LanguageServer for Backend {
             .to_file_path()
             .unwrap()
             .with_extension("pdf");
-
-        let output = match typst::compile(world) {
+        let messages: Vec<_> = match typst::compile(world) {
             Ok(document) => {
                 let buffer = typst::export::pdf(&document);
-                fs::write(output_path, buffer).map_err(|_| "failed to write PDF file".to_string())
+                let _ = fs::write(output_path, buffer).map_err(|_| "failed to write PDF file".to_string());
+                vec![]
             }
-            Err(errors) => {
-                let messages: Vec<_> = errors.iter().map(|error| error.message.as_str()).collect();
-                Err(messages.join("\n"))
-            }
+            Err(errors) => errors.iter().map(
+                |x| error_to_range(x, world)
+            ).collect(),
         };
+        drop(world);
 
         self.client
-            .log_message(MessageType::INFO, format!("{:?}", output))
+            .publish_diagnostics(
+                params.text_document.uri,
+                messages
+                    .into_iter()
+                    .map(|(message, range)| Diagnostic {
+                        range,
+                        severity: Some(DiagnosticSeverity::ERROR),
+                        message,
+                        ..Default::default()
+                    })
+                    .collect(),
+                None,
+            )
             .await;
     }
 
@@ -100,4 +115,24 @@ async fn main() {
         world: Arc::new(RwLock::new(None)),
     });
     Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+fn error_to_range(error: &SourceError, world: &SystemWorld) -> (String, Range) {
+    let source = world.source(error.span.source());
+    let range = source.range(error.span);
+    let range = range_to_lsp_range(range, source);
+    (error.message.to_string(), range)
+}
+
+fn range_to_lsp_range(range : std::ops::Range<usize>, source : &Source) -> Range {
+    Range {
+        start: Position {
+            line: source.byte_to_line(range.start).unwrap() as _,
+            character: source.byte_to_column(range.start).unwrap() as _,
+        },
+        end: Position {
+            line: source.byte_to_line(range.end).unwrap() as _,
+            character: source.byte_to_column(range.end).unwrap() as _,
+        },
+    }
 }
