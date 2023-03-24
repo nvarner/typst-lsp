f4eee0f908ebfcdd953f209833cab57827cf22b1
Valentin Bersier <vbersier@gmail.com>
2023-03-24 22:01:21 +0100

    chore: add prettier and format files

---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // same options as rust-analyzer, otherwise defaults from prettier
+    printWidth: 100,
+};
--- addons/vscode/.eslintrc.js
+++ addons/vscode/.eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: ["airbnb-base", "airbnb-typescript/base"],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'tsconfig.json'
-  },
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
-  }
-}
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "tsconfig.json",
+    },
+    rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+};
