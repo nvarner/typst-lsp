1ff20217c44ade4fcfcca1840b392980746d4a03
nvarner <nathanmvarner@protonmail.com>
2023-03-24 18:10:02 -0400

    further tighten eslint errors

--- addons/vscode/.eslintrc.js
+++ addons/vscode/.eslintrc.js
@@ -18,5 +18,21 @@ module.exports = {
     rules: {
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        "class-methods-use-this": "off",
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "init-declarations": "off",
+        "@typescript-eslint/init-declarations": "error",
+        "no-undef-init": "off",
+        "@typescript-eslint/strict-boolean-expressions": [
+            "error",
+            {
+                allowString: false,
+                allowNumber: false,
+                allowNullableObject: false,
+                allowNullableEnum: false,
+            },
+        ],
     },
 };
