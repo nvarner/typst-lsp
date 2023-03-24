7cc296ea6345410c07fff314d78376dcc4345025
Valentin Bersier <vbersier@gmail.com>
2023-03-24 22:01:21 +0100

    chore: add prettier and format files

--- addons/vscode/src/extension.ts
+++ addons/vscode/src/extension.ts
@@ -1,65 +1,59 @@
-import { type ExtensionContext } from 'vscode';
+import { type ExtensionContext } from "vscode";
 import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
 
-
 import {
-  LanguageClient,
-  type LanguageClientOptions,
-  type ServerOptions,
-} from 'vscode-languageclient/node';
+    LanguageClient,
+    type LanguageClientOptions,
+    type ServerOptions,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(_context: ExtensionContext) {
-  const serverCommand = getServer();
-  if (!serverCommand) return;
+    const serverCommand = getServer();
+    if (!serverCommand) return;
 
-  const serverOptions: ServerOptions = {
-    run: { command: serverCommand },
-    debug: { command: serverCommand },
-  };
+    const serverOptions: ServerOptions = {
+        run: { command: serverCommand },
+        debug: { command: serverCommand },
+    };
 
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'typst' }],
-  };
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "typst" }],
+    };
 
-  client = new LanguageClient(
-    'typst-lsp',
-    'Typst Language Server',
-    serverOptions,
-    clientOptions,
-  );
+    client = new LanguageClient("typst-lsp", "Typst Language Server", serverOptions, clientOptions);
 
-  client.start();
+    client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
-  }
-  return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
 
 function getServer(): string {
-  const windows = process.platform === "win32";
-  const suffix = windows ? ".exe" : "";
+    const windows = process.platform === "win32";
+    const suffix = windows ? ".exe" : "";
 
-  const bundledPath = path.resolve(__dirname, "typst-lsp" + suffix);
+    const bundledPath = path.resolve(__dirname, "typst-lsp" + suffix);
 
-  if (fileExists(bundledPath)) {
-    return bundledPath;
-  }
+    if (fileExists(bundledPath)) {
+        return bundledPath;
+    }
 
-  return "typst-lsp" + suffix;
+    return "typst-lsp" + suffix;
 }
 
 function fileExists(path: string) {
-  try {
-    fs.accessSync(path);
-    return true;
-  } catch (error) {
-    return false;
-  }
+    try {
+        fs.accessSync(path);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
