a43613bc6d1c5f434141b78d9e3507a6fd0ebe5f
nvarner <nathanmvarner@protonmail.com>
2023-04-06 04:24:58 -0400

    create ResourceManager to handle non-Typst-source files

--- src/workspace/mod.rs
+++ src/workspace/mod.rs
@@ -1,5 +1,7 @@
 //! Holds types relating to the LSP concept of a "workspace". That is, the directories a user has
 //! open in their editor, the files in them, the files they're currently editing, and so on.
 
+pub mod resource;
+pub mod resource_manager;
 pub mod source;
 pub mod source_manager;
