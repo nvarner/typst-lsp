0ce6aa80165a5f66e1631d943cd9725afc449163
nvarner <nathanmvarner@protonmail.com>
2023-04-06 04:07:07 -0400

    create SourcesManager to handle typst source code

---
+++ src/workspace/mod.rs
@@ -0,0 +1,5 @@
+//! Holds types relating to the LSP concept of a "workspace". That is, the directories a user has
+//! open in their editor, the files in them, the files they're currently editing, and so on.
+
+pub mod source;
+pub mod source_manager;
