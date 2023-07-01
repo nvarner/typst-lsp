20598441d86c6e9ba78d0ae4979e948407a60171
nvarner <nathanmvarner@protonmail.com>
2023-07-01 17:03:29 -0400

    make jaeger an optional feature

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use logging::{tracing_init, tracing_shutdown};
 use server::log::LspLayer;
 use server::TypstServer;
 use tower_lsp::{LspService, Server};
-use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{reload, Registry};
 
 mod command;
 mod config;
 mod ext;
+mod logging;
 mod lsp_typst_boundary;
 mod server;
 mod workspace;
@@ -16,7 +16,7 @@ mod workspace;
 async fn main() {
     let lsp_tracing_layer_handle = tracing_init();
     run(lsp_tracing_layer_handle).await;
-    opentelemetry::global::shutdown_tracer_provider();
+    tracing_shutdown();
 }
 
 #[tracing::instrument(skip_all)]
@@ -28,24 +28,4 @@ async fn run(lsp_tracing_layer_handle: reload::Handle<Option<LspLayer>, Registry
         LspService::new(move |client| TypstServer::new(client, lsp_tracing_layer_handle));
 
     Server::new(stdin, stdout, socket).serve(service).await;
-}
-
-fn tracing_init() -> reload::Handle<Option<LspLayer>, Registry> {
-    opentelemetry::global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
-
-    let tracer = opentelemetry_jaeger::new_collector_pipeline()
-        .with_endpoint("http://localhost:14268/api/traces")
-        .with_service_name("typst-lsp")
-        .with_isahc()
-        .install_batch(opentelemetry::runtime::Tokio)
-        .ok();
-
-    let (lsp_layer, lsp_layer_handle) = reload::Layer::new(None);
-
-    tracing_subscriber::registry()
-        .with(lsp_layer)
-        .with(tracer.map(|tracer| tracing_opentelemetry::layer().with_tracer(tracer)))
-        .init();
-
-    lsp_layer_handle
 }
