7171aa7ccfcf9da33e77f968ed8caac38a7d882b
nvarner <nathanmvarner@protonmail.com>
2023-08-10 16:51:04 -0400

    add version flag (#211)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bpaf::{construct, OptionParser, Parser};
 use logging::{tracing_init, tracing_shutdown};
 use server::log::LspLayer;
 use server::TypstServer;
@@ -12,6 +13,9 @@ mod lsp_typst_boundary;
 mod server;
 mod workspace;
 
+// TODO: is there a good way to get this automatically at build time?
+pub const TYPST_VERSION: &str = "0.6.0";
+
 #[tokio::main]
 async fn main() {
     let lsp_tracing_layer_handle = tracing_init();
@@ -21,6 +25,8 @@ async fn main() {
 
 #[tracing::instrument(skip_all)]
 async fn run(lsp_tracing_layer_handle: reload::Handle<Option<LspLayer>, Registry>) {
+    let _args = arg_parser().run();
+
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
@@ -28,4 +34,17 @@ async fn run(lsp_tracing_layer_handle: reload::Handle<Option<LspLayer>, Registry
         LspService::new(move |client| TypstServer::new(client, lsp_tracing_layer_handle));
 
     Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+#[derive(Debug, Clone)]
+struct Args {}
+
+fn arg_parser() -> OptionParser<Args> {
+    construct!(Args {}).to_options().version(
+        format!(
+            "{} (Typst version {TYPST_VERSION})",
+            env!("CARGO_PKG_VERSION")
+        )
+        .as_str(),
+    )
 }
