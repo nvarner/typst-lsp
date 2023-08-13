950a440105da8f621a2ba0e3dccd975a66c114de
nvarner <nathanmvarner@protonmail.com>
2023-08-13 14:52:15 -0400

    reimplement PDF URI function in more generality

--- src/server/export.rs
+++ src/server/export.rs
@@ -3,7 +3,7 @@ use tower_lsp::lsp_types::Url;
 use tracing::info;
 use typst::doc::Document;
 
-use crate::workspace::fs::FsResult;
+use crate::ext::UrlExt;
 
 use super::TypstServer;
 
@@ -11,7 +11,7 @@ impl TypstServer {
     #[tracing::instrument(skip(self))]
     pub async fn export_pdf(&self, source_uri: &Url, document: Document) -> anyhow::Result<()> {
         let data = self.thread(move |_| typst::export::pdf(&document)).await;
-        let pdf_uri = self.pdf_uri(source_uri).await?;
+        let pdf_uri = source_uri.clone().with_extension("pdf")?;
         info!(%pdf_uri, "exporting PDF");
 
         let thread_uri = pdf_uri.clone();
@@ -27,12 +27,5 @@ impl TypstServer {
         info!("PDF export complete");
 
         Ok(())
-    }
-
-    async fn pdf_uri(&self, source_uri: &Url) -> FsResult<Url> {
-        let (project, full_id) = self.project_and_full_id(source_uri).await?;
-        let pdf_full_id = full_id.with_extension("pdf");
-        let pdf_uri = project.full_id_to_uri(pdf_full_id).await?;
-        Ok(pdf_uri)
     }
 }
