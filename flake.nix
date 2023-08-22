10819b83024b6b34b63823791a72cbbe10b2d8e7
mangoiv <mail@mangoiv.com>
2023-08-22 14:01:11 +0200

    [fix] adjust to new version - some improvements on the flake (improve caching) - pin inputs to the proper versions as mandated by the   Cargo.toml - copy `README.md` to the place that typst-fmt requires it

--- flake.nix
+++ flake.nix
@@ -1,88 +1,92 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    fenix.url = "github:nix-community/fenix";
+    nixpkgs.follows = "fenix/nixpkgs";
+    # it makes more sense for us to follow the nixpkgs of fenix than making
+    # fenix follow nixpkgs because nix-community's cachix probably has our toolchain
+    # cached, we don't want to unnecessarily build it.
     typst = {
-      url = "github:typst/typst";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.fenix.follows = "fenix";
+      url = "github:typst/typst/v0.7.0";
+      flake = false;
+    };
+    typst-fmt = {
+      url = "github:astrale-sharp/typstfmt/8404a15";
       flake = false;
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, nixpkgs, typst, flake-utils }:
-    let
-      inherit (nixpkgs.lib) importTOML optionals cleanSource;
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        fenixPkgs = fenix.packages.${system};
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ {
+    fenix,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    inherit (nixpkgs.lib) importTOML optionals cleanSource;
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      fenixPkgs = fenix.packages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        toolchain = fenixPkgs.minimal.toolchain;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = toolchain;
-          rustc = toolchain;
+      toolchain = fenixPkgs.minimal.toolchain;
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          (fenixPkgs.default.withComponents [
+            "cargo"
+            "clippy"
+            "rustc"
+            "rustfmt"
+          ])
+          fenixPkgs.rust-analyzer
+          pkgs.nodejs
+        ];
+
+        buildInputs = optionals pkgs.stdenv.isDarwin [
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+          pkgs.libiconv
+        ];
+
+        inherit PKG_CONFIG_PATH;
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+
+        # Required by the jaeger feature
+        nativeBuildInputs = [pkgs.pkg-config];
+      };
+
+      packages.default = rustPlatform.buildRustPackage {
+        pname = "typst-lsp";
+
+        inherit PKG_CONFIG_PATH;
+        inherit ((importTOML ./Cargo.toml).package) version;
+
+        doCheck = false;
+
+        src = cleanSource ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          allowBuiltinFetchGit = true;
         };
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            (fenixPkgs.default.withComponents [
-              "cargo"
-              "clippy"
-              "rustc"
-              "rustfmt"
-            ])
-            fenixPkgs.rust-analyzer
-            pkgs.nodejs
-          ];
 
-          buildInputs = optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.CoreServices
-            pkgs.libiconv
-          ];
+        preBuild = ''
+          cp -r ${inputs.typst}/assets /build/cargo-vendor-dir/assets
+          cp -r ${inputs.typst-fmt}/README.md /build/cargo-vendor-dir
+        '';
 
-          inherit PKG_CONFIG_PATH;
-          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        nativeBuildInputs = [
+          pkgs.installShellFiles
+          pkgs.pkg-config
+        ];
 
-          # Required by the jaeger feature
-          nativeBuildInputs = [ pkgs.pkg-config ];
-        };
-
-        packages.default = rustPlatform.buildRustPackage {
-          pname = "typst-lsp";
-
-          inherit PKG_CONFIG_PATH;
-          inherit ((importTOML ./Cargo.toml).package) version;
-
-          doCheck = false;
-
-          src = cleanSource ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            allowBuiltinFetchGit = true;
-          };
-
-          preBuild = ''
-            cp -r ${typst}/assets /build/cargo-vendor-dir/assets
-          '';
-
-          nativeBuildInputs = [
-            pkgs.installShellFiles
-            pkgs.pkg-config
-          ];
-
-          buildInputs = optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.CoreServices
-          ];
-        };
-      });
-
+        buildInputs = optionals pkgs.stdenv.isDarwin [
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+        ];
+      };
+    });
 }
