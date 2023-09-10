90e69ce37ac7e862e3d27717277e0e47df88f09b
KillTheMule <KillTheMule@users.noreply.github.com>
2023-09-10 23:41:21 +0200

    Read typstfmt config from files, if possible

--- src/server/formatting.rs
+++ src/server/formatting.rs
@@ -1,10 +1,14 @@
+use std::{fs::File, io::Read};
+
 use tower_lsp::lsp_types::{Position, Range, Registration, TextEdit, Unregistration};
 use typst::syntax::Source;
+use typstfmt_lib::Config as FmtConfig;
 
 use super::TypstServer;
 
 const FORMATTING_REGISTRATION_ID: &str = "formatting";
 const DOCUMENT_FORMATTING_METHOD_ID: &str = "textDocument/formatting";
+const CONFIG_PATH: &str = "typstfmt-config.toml";
 
 pub fn get_formatting_registration() -> Registration {
     Registration {
@@ -24,7 +28,7 @@ pub fn get_formatting_unregistration() -> Unregistration {
 impl TypstServer {
     pub fn format_document(&self, source: &Source) -> anyhow::Result<Vec<TextEdit>> {
         let original_text = source.text();
-        let res = typstfmt_lib::format(original_text, typstfmt_lib::Config::default());
+        let res = typstfmt_lib::format(original_text, self.get_fmt_config());
 
         Ok(vec![TextEdit {
             new_text: res,
@@ -39,5 +43,28 @@ impl TypstServer {
                 },
             ),
         }])
+    }
+
+    fn get_fmt_config(&self) -> FmtConfig {
+        // Ignoring all errors since we're returning the default config in case
+        // we can't find something more specific
+        let mut config_file: Option<File> = File::options().read(true).open(CONFIG_PATH).ok();
+
+        if config_file.is_none() {
+            if let Some(root_path) = &self.config.blocking_read().root_path {
+                let mut root_path = root_path.clone();
+                root_path.push(CONFIG_PATH);
+                config_file = File::options().read(true).open(root_path).ok();
+            }
+        }
+
+        config_file
+            .map(|mut f| {
+                let mut buf = String::default();
+                let _ = f.read_to_string(&mut buf);
+                FmtConfig::from_toml(&buf).ok()
+            })
+            .flatten()
+            .unwrap_or_default()
     }
 }
